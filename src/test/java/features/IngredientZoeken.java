a12909c00e6fbb2a6d86caff644a4b1747def275
Luca Dandois <luca.dandois@student.kdg.be>
2023-12-18 00:02:33 +0100

    started working on IngredientZoeken

---
+++ src/test/java/features/IngredientZoeken.java
@@ -0,0 +1,9 @@
+package features;
+
+import io.cucumber.java.en.When;
+
+public class IngredientZoeken {
+    @When("ik zoek op het woord {string}")
+    public void ikZoekOpHetWoord(String arg0) {
+    }
+}
